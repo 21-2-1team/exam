277e209542c13a5ad63a87a7cb3136608930fd97
NA YONGWOO <na7149@gmail.com>
2021-06-16 18:04:52 +0900

    edit

--- src/main/java/bidding/external/BiddingManagementService.java
+++ src/main/java/bidding/external/BiddingManagementService.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name="BiddingManagement", url="http://BiddingManagement:8080")
+@FeignClient(name="BiddingManagement", url="http://localhost:8081")
 public interface BiddingManagementService {
 
     @RequestMapping(method= RequestMethod.PATCH, path="/biddingManagements")
